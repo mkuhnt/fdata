c69214950c02acb80fb7cc5df18f7aef40559376
Markus Kuhnt <markus.kuhnt@gmail.com>
2015-05-01 17:19:39 +0200

    adds dimension range to stores

--- typing.hs
+++ typing.hs
@@ -1,36 +1,45 @@
 import qualified Data.Map as Map
 
 data Qualification = Qualification Int Int deriving (Eq, Ord, Show)
-data Store = Store Int (Map.Map Qualification Store) | Point Int deriving (Show)
+data Store = Store Int Int (Map.Map Qualification Store) | Point Int deriving (Show)
 
 
 create :: Int -> Store
-create n
+create n = createIndexed n 1
+
+
+createIndexed :: Int -> Int -> Store
+createIndexed n idx
   | n <= 0 = Point 0
-  | n >= 1 = Store n Map.empty
+  | n >= 1 = Store n idx Map.empty
 
 
 dimensionality :: Store -> Int
 dimensionality (Point _) = 0
-dimensionality (Store n _) = n
+dimensionality (Store n _ _) = n
+
+
+dimensions :: Store -> [Int]
+dimensions (Point _) = []
+dimensions (Store n start _) = [start..(start + n - 1)]
 
 
 slice :: Store -> Qualification -> Store
 slice (Point _) _  = error "You cannot slice a point. It is atomic ;)"
-slice (Store n m) q
+slice (Store n i m) q
   | n <= 0 = error "A store with zero dimensions is a point and cannot be sliced."
   | n == 1 = case Map.lookup q m of
     Nothing  -> Point 0
     Just val -> val
   | n >= 2 = case Map.lookup q m of
-    Nothing  -> create (n-1)
+    Nothing  -> createIndexed (n-1) (i+1)
     Just val -> val
 
 
 splice :: Store -> Store -> Qualification -> Store
-splice (Point v) (Store 1 m) q = Store 1 (Map.insert q (Point v) m)
-splice (Store d1 m1) (Store d2 m2) q
-  | d1 == (d2 - 1) = Store d2 (Map.insert q (Store d1 m1) m2)
+splice (Point v) (Store 1 i m) q = Store 1 i (Map.insert q (Point v) m)
+splice (Store d1 i1 m1) (Store d2 i2 m2) q
+  | d1 == (d2 - 1) = Store d2 i2 (Map.insert q (Store d1 i1 m1) m2)
   | otherwise      = error "You cannot splice these two stores."
 
 
